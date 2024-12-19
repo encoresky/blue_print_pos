712446f658951e3226d4055e9cd6899699aff118
Ankush Lokhande <ankush.lokhande@encoresky.com>
2024-12-19 15:04:35 +0530

    Modify printing binary data format

--- lib/blue_print_pos.dart
+++ lib/blue_print_pos.dart
@@ -244,7 +244,7 @@ class BluePrintPos {
     final CapabilityProfile profile = await CapabilityProfile.load();
     final Generator generator = Generator(paperSize, profile);
     final img.Image _resize = img.copyResize(
-      img.decodeImage(data)!,
+      img.decodeImage(Uint8List.fromList(data))!,
       width: customWidth > 0 ? customWidth : paperSize.width,
     );
     if (useRaster) {
