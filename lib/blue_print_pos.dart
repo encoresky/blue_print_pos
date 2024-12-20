fc6e22b9ba78cd4b34e7cd90dc9e21e960f0e76c
Ankush Lokhande <ankush.lokhande@encoresky.com>
2024-12-20 17:39:04 +0530

    Fix imports

--- lib/blue_print_pos.dart
+++ lib/blue_print_pos.dart
@@ -1,13 +1,15 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:blue_print_pos/models/models.dart';
+import 'package:blue_print_pos/receipt/receipt.dart';
 import 'package:blue_print_pos/receipt/receipt_section_text.dart';
 import 'package:blue_print_pos/scanner/blue_scanner.dart';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart' as blue_thermal;
 import 'package:esc_pos_utils_plus/esc_pos_utils.dart';
+// import 'package:esc_pos_utils_plus/esc_pos_utils_plus.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:image/image.dart' as img;
